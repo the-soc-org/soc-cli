183f112ace02f8088718e4e2d03b8c7dd28eb109
RafalSz1 <332718@stud.umk.pl>
2026-08-14 03:52:54 +0200

    refactor: extract bash status constants to shared library

---
+++ soc-lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Define visual indicators for the terminal output to enhance readability.
+# - `OK`: White Heavy Check Mark (Unicode: U+2705), indicates success.
+# - `YUP`: Check Mark (Unicode: U+2713), also indicates confirmation or success.
+# - `WRN`: High Voltage Sign (Unicode: U+26A1), used to signal warnings or cautions.
+# - `ERR`: Cross Mark (Unicode: U+274C), indicates errors or failure.
+OK=✅
+YUP=✓
+WRN=⚡
+ERR=❌
